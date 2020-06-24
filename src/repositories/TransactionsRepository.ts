4896683c1f9cc7c93413e0c7d87abd2f5cc2b808
Luis Rodrigues <luisitaloar@gmail.com>
2020-06-24 13:12:39 -0300

    Desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,20 +9,37 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { total: 0, outcome: 0, income: 0 };
   }
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    this.balance = { total: 0, outcome: 0, income: 0 };
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        this.balance.income += transaction.value;
+      } else {
+        this.balance.outcome += transaction.value;
+      }
+    });
+    this.balance.total = this.balance.income - this.balance.outcome;
+    return this.balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
     // TODO
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('type deve ser income ou outcome');
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('Você não tem saldo suficiente');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
